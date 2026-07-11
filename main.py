e5a143f266ffe38140447c7f70eaec9d83f69c1c
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 02:55:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="BTT Platform",
+    version="0.1.0",
+)
+
+@app.get("/")
+async def root():
+    return {
+        "project": "BTT",
+        "status": "running",
+        "version": "0.1.0"
+    }
