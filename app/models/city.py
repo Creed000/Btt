24da3661146509402dc77f211ebbc324ba4b0030
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 12:54:16 +0300

    Create city.py

---
+++ app/models/city.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+    )
+
+    is_active: Mapped[bool] = mapped_column(default=True)
