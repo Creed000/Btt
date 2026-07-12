4d6140bc0cedcdd73e07c25372425de390bd41c3
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 03:18:15 +0300

    Implement user registration in UserService

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,1 +1,25 @@
+from sqlalchemy.orm import Session
 
+from app.repositories.user_repository import UserRepository
+
+
+class UserService:
+
+    @staticmethod
+    def register(db: Session, tg_user):
+
+        user = UserRepository.get_by_telegram_id(
+            db,
+            tg_user.id,
+        )
+
+        if user:
+            return user
+
+        return UserRepository.create(
+            db=db,
+            telegram_id=tg_user.id,
+            username=tg_user.username,
+            first_name=tg_user.first_name,
+            last_name=tg_user.last_name,
+        )
