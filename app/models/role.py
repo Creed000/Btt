0fb43c2169bc4f3b1be6b494faa670fee88a4bcb
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 06:51:25 +0300

    Update role.py

--- app/models/role.py
+++ app/models/role.py
@@ -1,18 +1,25 @@
-from sqlalchemy import Integer, String
+from enum import Enum
+
+from sqlalchemy import Enum as SqlEnum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database.base import Base
 
 
+class UserRole(str, Enum):
+    CLIENT = "client"
+    MASTER = "master"
+    SALON_ADMIN = "salon_admin"
+    SUPER_ADMIN = "super_admin"
+
+
 class Role(Base):
     __tablename__ = "roles"
 
-    id: Mapped[int] = mapped_column(
-        Integer,
-        primary_key=True
-    )
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    name: Mapped[str] = mapped_column(
-        String(50),
-        unique=True
+    name: Mapped[UserRole] = mapped_column(
+        SqlEnum(UserRole),
+        unique=True,
+        nullable=False,
     )
