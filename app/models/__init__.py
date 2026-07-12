629c7fd12dd80836116b5fcf13b298a1d9930b53
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 21:21:39 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,15 @@
 from .user import User
 from .role import Role
+from .city import City
+from .master import Master
+from .service import Service
+from .booking import Booking
 
 __all__ = [
     "User",
     "Role",
+    "City",
+    "Master",
+    "Service",
+    "Booking",
 ]
