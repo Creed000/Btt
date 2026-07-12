1064aa2d30062c3456a0b9d05809c3d50d13b65c
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 06:04:02 +0300

    Update database connection for PostgreSQL driver

--- app/database/session.py
+++ app/database/session.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import make_url
 from sqlalchemy.orm import sessionmaker
 
 from app.config.settings import settings
 
+url = make_url(settings.DATABASE_URL)
+
+if url.drivername == "postgresql":
+    url = url.set(drivername="postgresql+psycopg")
+
 engine = create_engine(
-    settings.DATABASE_URL,
+    url,
     echo=settings.DEBUG,
 )
 
