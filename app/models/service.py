21e31753d8fad2b0c6dcb47a76ac888d796d1a23
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 12:56:35 +0300

    Enhance service model with relationships
    Added relationships for master and category in the service model.

--- app/models/service.py
+++ app/models/service.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.base import Base
 
@@ -10,17 +10,29 @@ class Service(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
 
     master_id: Mapped[int] = mapped_column(
-        ForeignKey("masters.id")
+        ForeignKey("masters.id"),
+        nullable=False,
+    )
+
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id"),
+        nullable=False,
     )
 
     title: Mapped[str] = mapped_column(
-        String(100)
+        String(100),
+        nullable=False,
     )
 
     duration: Mapped[int] = mapped_column(
-        Integer
+        Integer,
+        nullable=False,
     )
 
     price: Mapped[int] = mapped_column(
-        Integer
+        Integer,
+        nullable=False,
     )
+
+    master = relationship("Master", back_populates="services")
+    category = relationship("Category", back_populates="services")
