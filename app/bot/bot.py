6cf2b3de12433bd2d10a2dacb8b3f3d890e92056
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-12 03:09:46 +0300

    Update bot.py

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,9 +1,12 @@
 from telegram.ext import Application
 
 from app.config.settings import settings
+from app.handlers.start import start_handler
 
 application = (
     Application.builder()
     .token(settings.BOT_TOKEN)
     .build()
 )
+
+application.add_handler(start_handler)
