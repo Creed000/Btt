c2c323ec3e67bdebe17f59f2c728343d33e1bfbf
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-14 02:37:13 +0300

    Remove unused handlers from bot application
    Removed profile and booking handlers from the bot application.

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -3,10 +3,7 @@ from telegram.ext import Application
 from app.config.settings import settings
 
 from app.handlers.start import start_handler
-from app.handlers.profile import profile_handler
 from app.handlers.menu import menu_handler
-from app.handlers.booking import booking_handler
-
 
 application = (
     Application.builder()
@@ -14,12 +11,5 @@ application = (
     .build()
 )
 
-# Команды
 application.add_handler(start_handler)
-application.add_handler(profile_handler)
-
-# Главное меню
 application.add_handler(menu_handler)
-
-# Запись к мастеру
-application.add_handler(booking_handler)
