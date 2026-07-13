8316b1b2eb96ec7ab5f65c3d4833631cd7a8bb53
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-14 01:16:21 +0300

    Add booking handler to the bot application

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -5,6 +5,8 @@ from app.config.settings import settings
 from app.handlers.start import start_handler
 from app.handlers.profile import profile_handler
 from app.handlers.menu import menu_handler
+from app.handlers.booking import booking_handler
+
 
 application = (
     Application.builder()
@@ -16,5 +18,8 @@ application = (
 application.add_handler(start_handler)
 application.add_handler(profile_handler)
 
-# Кнопки главного меню
+# Главное меню
 application.add_handler(menu_handler)
+
+# Запись к мастеру
+application.add_handler(booking_handler)
