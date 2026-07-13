f068fbf45deb21ad137cc311646884e148d5ced5
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-14 02:43:41 +0300

    Add new handlers for booking, profile, search, and settings

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -4,6 +4,10 @@ from app.config.settings import settings
 
 from app.handlers.start import start_handler
 from app.handlers.menu import menu_handler
+from app.handlers.booking import booking_handler
+from app.handlers.profile import profile_handler
+from app.handlers.search import search_handler
+from app.handlers.settings import settings_handler
 
 application = (
     Application.builder()
@@ -11,5 +15,10 @@ application = (
     .build()
 )
 
+# Регистрация обработчиков
 application.add_handler(start_handler)
 application.add_handler(menu_handler)
+application.add_handler(booking_handler)
+application.add_handler(profile_handler)
+application.add_handler(search_handler)
+application.add_handler(settings_handler)
