c8de0ca00e645ca3ef9e7767b50861d6b795e4a2
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-15 01:09:25 +0300

    Configure SQLAlchemy engine connection pool settings

--- app/database/session.py
+++ app/database/session.py
@@ -12,6 +12,10 @@ if url.drivername == "postgresql":
 engine = create_engine(
     url,
     echo=settings.DEBUG,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
 )
 
 SessionLocal = sessionmaker(
