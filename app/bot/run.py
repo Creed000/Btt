0d51cabfb16a9a434e5c1c270e4963072234b305
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-07-17 00:56:25 +0300

    Update run.py

--- app/bot/run.py
+++ app/bot/run.py
@@ -1,4 +1,6 @@
+from app.database.init_db import init_db
 from app.bot.bot import application
 
 if __name__ == "__main__":
+    init_db()          # создаёт все таблицы
     application.run_polling()
