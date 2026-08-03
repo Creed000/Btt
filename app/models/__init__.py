0b20b499c48c54736d9b1b3b84381cee22710d42
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-08-04 01:58:39 +0300

    Refactor imports in __init__.py for models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,17 +1,22 @@
-from .user import User
-from .role import Role
-from .city import City
-from .category import Category
-from .master import Master
-from .client import Client
-from .service import Service
 from .booking import Booking
+from .branch import Branch
+from .category import Category
+from .city import City
+from .client import Client
+from .master import Master
+from .role import Role
+from .salon import Salon
+from .service import Service
+from .user import User
+
 
 __all__ = [
     "User",
     "Role",
     "City",
     "Category",
+    "Salon",
+    "Branch",
     "Master",
     "Client",
     "Service",
