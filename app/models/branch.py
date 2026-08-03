f86042303fad3e58c7bf5f4299d9c8df5bbc552c
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-08-04 02:10:31 +0300

    Refactor Branch model with constraints and relationships
    Added nullable constraints to the 'name' and 'address' fields. Introduced a relationship with the 'Salon' model.

--- app/models/branch.py
+++ app/models/branch.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.base import Base
 
@@ -7,12 +7,30 @@ from app.database.base import Base
 class Branch(Base):
     __tablename__ = "branches"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-
-    salon_id: Mapped[int] = mapped_column(
-        ForeignKey("salons.id")
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
     )
 
-    name: Mapped[str] = mapped_column(String(100))
+    salon_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "salons.id",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+        index=True,
+    )
 
-    address: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+    )
+
+    address: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    salon = relationship(
+        "Salon",
+        back_populates="branches",
+    )
