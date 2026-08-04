fc5a977d6172fc940e72b2179274006786ccc0b0
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-08-05 02:41:55 +0300

    Refactor imports and add logging for timezone
    Updated import paths and added logging for application timezone.

--- run_bot.py
+++ run_bot.py
@@ -3,13 +3,24 @@ import logging
 from telegram import Update
 
 from app.bot.bot import application
-from app.bot.run import prepare_database
+from app.database.prepare import prepare_database
+from app.services.timezone import APP_TIMEZONE
 
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    level=logging.INFO,
+)
 
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    logger.info(
+        "Часовой пояс приложения: %s",
+        APP_TIMEZONE,
+    )
+
     prepare_database()
 
     logger.info(
