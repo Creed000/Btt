47efa450b3a05786b721e833555bc7ccf212e55b
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-08-04 23:29:32 +0300

    Enhance bot startup with logging and database prep
    Added logging and database preparation before starting the bot.

--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,23 @@
+import logging
+
+from telegram import Update
+
 from app.bot.bot import application
+from app.bot.run import prepare_database
+
+
+logger = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
-    print("🚀 BTT Bot started")
-    application.run_polling()
+    prepare_database()
+
+    logger.info(
+        "Запуск Telegram-бота через run_bot.py"
+    )
+
+    application.run_polling(
+        allowed_updates=Update.ALL_TYPES,
+        drop_pending_updates=True,
+        bootstrap_retries=-1,
+    )
