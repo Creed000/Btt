ade860de2dc80b550af073665d7c7ab353e85102
Creed000 <97703482+Creed000@users.noreply.github.com>
2026-08-08 06:07:55 +0600

    Refactor run_bot.py to simplify execution
    Removed logging and database preparation from run_bot.py.

--- run_bot.py
+++ run_bot.py
@@ -1,34 +1,5 @@
-import logging
-
-from telegram import Update
-
-from app.bot.bot import application
-from app.database.prepare import prepare_database
-from app.services.timezone import APP_TIMEZONE
-
-
-logging.basicConfig(
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    level=logging.INFO,
-)
-
-logger = logging.getLogger(__name__)
+from app.bot.run import run
 
 
 if __name__ == "__main__":
-    logger.info(
-        "Часовой пояс приложения: %s",
-        APP_TIMEZONE,
-    )
-
-    prepare_database()
-
-    logger.info(
-        "Запуск Telegram-бота через run_bot.py"
-    )
-
-    application.run_polling(
-        allowed_updates=Update.ALL_TYPES,
-        drop_pending_updates=True,
-        bootstrap_retries=-1,
-    )
+    run()
